5c623dcf0f082a8863cb837d0ae89f4a0ea80776
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2019-09-21 18:43:29 +0300

    Add brew packages to mac bootstrap script

--- bootstrap/mac.sh
+++ bootstrap/mac.sh
@@ -3,4 +3,22 @@
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-
+# packages
+brew install \
+    antibody \
+    bat \
+    cloc \
+    curl \
+    diff-so-fancy \
+    fzf \
+    git \
+    go \
+    gnupg \
+    jq \
+    httpie \
+    lazygit \
+    pinentry-mac \
+    tree \
+    wget \
+    zsh \
+    zsh-autosuggestions
